6cb97fc7e0b23d6b591999e7b68c73baea12dcd7
Lucas Maliszewski <lucas.maliszewski@a2zsync.com>
2022-02-06 10:41:32 -0700

    Updating some of the logic.

--- python/sort/merge/merge_sort_multi_methods.py
+++ python/sort/merge/merge_sort_multi_methods.py
@@ -1,6 +1,16 @@
 import random
 
-def mergeSortMicroSort(list_of_items: list) -> list:
+def mergeSortMicroSort(list_of_items: list):
+    """Merge Sort Micro Sort.
+
+    This is the simple sorting of smallest to largest.
+
+    Args:
+        list_of_items (list): Contains the list of numeric items
+
+    Returns:
+        list: The sorted items
+    """
     swapped = True
     index = 0
     while swapped:
@@ -12,37 +22,63 @@ def mergeSortMicroSort(list_of_items: list) -> list:
             if next_index > list_len - 1:
                 swapped = True
             elif i > list_of_items[next_index]:
-                list_of_items[index], list_of_items[next_index] = list_of_items[next_index], list_of_items[index]
+                list_of_items[index], list_of_items[next_index] = (
+                    list_of_items[next_index],
+                    list_of_items[index],
+                )
                 swapped = True
 
             index = next_index if list_len - 1 > next_index else 0
 
     return list_of_items
 
-def mergeSortMicroMerge(t: list)->list:
-    run = True
-    tmp = []
+
+def mergeSortMicroMerge(lists_to_be_merged: list):
+    """Merge Sort Micro Merge.
+
+    This is a merge helper, as the recursive values merge back into eachother.
+
+    Args:
+        lists_to_be_merged (list): List of lists with numeric values.
+
+    Returns:
+        list : List of numeric values in ascending order.
+    """
+    run_merge = True
+    new_compiled_list = []
     cursor = 0
 
-    while run:
-        if not len(t[cursor]) and not len(t[cursor + 1]):
+    while run_merge:
+        if not len(lists_to_be_merged[cursor]) and not len(lists_to_be_merged[cursor + 1]):
             cursor += 1
-            run = False if cursor >= len(t) - 1 else True
-            if run:
-                t[cursor] = tmp
-                tmp = []
-        elif not len(t[cursor]) and len(t[cursor + 1]):
-            tmp.append(t[cursor + 1].pop(0))
-        elif not len(t[cursor + 1]) and len(t[cursor]):
-            tmp.append(t[cursor].pop(0))
-        elif t[cursor][0] < t[cursor + 1][0]:
-            tmp.append(t[cursor].pop(0))
+            run_merge = False if cursor >= len(lists_to_be_merged) - 1 else True
+            if run_merge:
+                lists_to_be_merged[cursor] = new_compiled_list
+                new_compiled_list = []
+        elif not len(lists_to_be_merged[cursor]) and len(lists_to_be_merged[cursor + 1]):
+            new_compiled_list.append(lists_to_be_merged[cursor + 1].pop(0))
+        elif not len(lists_to_be_merged[cursor + 1]) and len(lists_to_be_merged[cursor]):
+            new_compiled_list.append(lists_to_be_merged[cursor].pop(0))
+        elif lists_to_be_merged[cursor][0] < lists_to_be_merged[cursor + 1][0]:
+            new_compiled_list.append(lists_to_be_merged[cursor].pop(0))
         else:
-            tmp.append(t[cursor + 1].pop(0))
+            new_compiled_list.append(lists_to_be_merged[cursor + 1].pop(0))
 
-    return tmp
+    return new_compiled_list
 
-def mergeSortSplit(list_of_items: list, split_by_3: bool) -> list:
+
+def mergeSortSplit(list_of_items: list, split_by_3: bool):
+    """Merge Sort Split.
+
+    Here is where we use recursion to break down the list of numbers given.
+
+    Args:
+        list_of_items (list): List of numeric values.
+        split_by_3 (bool): Split by 2 or 3 sections?
+
+    Returns:
+        list: List of numeric values in ascending order.
+    """
     split_it = 3 if split_by_3 else 2
     items_len = len(list_of_items)
     split = items_len // split_it
@@ -57,33 +93,18 @@ def mergeSortSplit(list_of_items: list, split_by_3: bool) -> list:
         return mergeSortMicroSort(list_of_items)
 
     if split_by_3:
-        i = split
-        j = split*2
-        a = list_of_items[:i]
-        b = list_of_items[i:j]
-        c = list_of_items[j:]
-        aa = list_of_items[:split]
-        bb = list_of_items[split:-split]
-        cc = list_of_items[-split:]
-        pause = True
-        t = [
-            mergeSortSplit(list_of_items[:i], split_by_3),
-            mergeSortSplit(list_of_items[i:j], split_by_3),
-            mergeSortSplit(list_of_items[j:], split_by_3)
-        ]
-    else:
-        a = list_of_items[:split]
-        c = list_of_items[split:]
         t = [
             mergeSortSplit(list_of_items[:split], split_by_3),
-            mergeSortSplit(list_of_items[split:], split_by_3)
+            mergeSortSplit(list_of_items[split: split * 2], split_by_3),
+            mergeSortSplit(list_of_items[split * 2:], split_by_3),
         ]
-
+    else:
+        t = [
+            mergeSortSplit(list_of_items[:split], split_by_3),
+            mergeSortSplit(list_of_items[split:], split_by_3),
+        ]
     # merge them
-    list_of_items = mergeSortMicroMerge(t)
-
-    # list
-    return list_of_items
+    return mergeSortMicroMerge(t)
 
 def mergeSort(list_of_items, split_by_3=False):
     list_of_items = mergeSortSplit(list_of_items, split_by_3)
@@ -91,7 +112,6 @@ def mergeSort(list_of_items, split_by_3=False):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # generate random nubmers
     print("Testing with even array")
     item_arr = [101, 43, 4, 5, 68, 6, 1, 9, 100, 20, 33, 22, 88, 3]
     final = item_arr.copy()
